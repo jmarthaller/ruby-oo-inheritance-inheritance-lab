72de765e7c29badbefc3120b34e443bea70ee4c3
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-26 07:05:59 -0500

    inheritence lab

---
+++ lib/user.rb
@@ -0,0 +1,21 @@
+class User
+
+    attr_accessor :first_name, :last_name
+    # def initialize(first_name, last_name)
+    #     @first_name = first_name
+    #     @last_name = last_name
+    # end
+
+    # def first_name
+    #     puts "#{self.first_name}"
+    # end
+
+    # def last_name
+    #     puts "#{self.last_name}"
+    # end
+
+    # def full_name
+    #     puts "#{self.first_name} #{self.last_name}"
+    # end
+
+end
